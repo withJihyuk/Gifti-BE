179659ab40bc658f03b8322a74f885c094f63b3c
Jihyuk Lee <lead@sver.dev>
2025-02-27 21:05:26 +0900

    init :: 프로젝트 시작

---
+++ src/main/java/org/gifti/team/backend/GiftiBackendApplication.java
@@ -0,0 +1,13 @@
+package org.gifti.team.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiftiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiftiBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/gifti/team/backend/GiftiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gifti.team.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiftiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
